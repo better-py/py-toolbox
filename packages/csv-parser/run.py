63997f2380ed6171320d207da9c2c290fd02fa68
Henry.Huang <hhglory@outlook.com>
2022-10-23 17:49:14 +0800

    update

--- packages/csv-parser/run.py
+++ packages/csv-parser/run.py
@@ -1,66 +1,54 @@
-import json
-import os
-import shutil
-from typing import Optional, Callable
-
 import click
-import web3
-from web3 import Web3
-from etherscan.accounts import Account
+import pyexcel as pe
 
 
 class CSVParser(object):
-    """
-    reference:
-        - etherscan.io api:
-            - https://docs.etherscan.io/misc-tools-and-utilities/using-this-docs
-            - https://github.com/corpetty/py-etherscan-api
-            - https://github.com/pcko1/etherscan-python
+    """表格处理工具
 
     """
 
-    def __init__(self, rpc_url: str, etherscan_api_key: str):
-        print(f"init eth, rpc url: {rpc_url}")
-        self.client = web3.Web3(Web3.HTTPProvider(rpc_url))
-        if not self.client.isConnected():
-            print(f"error: rpc not ok, invalid rpc url: {rpc_url}")
+    def __init__(self, file_a: str, file_b: str):
+        self.file_a = file_a or None
+        self.file_b = file_b or None
 
-        self.etherscan_api_key = etherscan_api_key
+        # sheet book:
+        self.book_a = None
+        self.book_b = None
 
-    def balance(self, address: str):
-        balance = self.client.eth.getBalance(address)
-        eth_balance = self.client.fromWei(balance, "ether")
+    def read(self, file: str):
+        if not file:
+            return None
 
-        print(f"address balance: {balance}, {eth_balance} ETH")
+        # read file:
+        book = pe.get_book(file_name=file)
+        # the default iterator for a **Book* instance is a SheetIterator
+        for sheet in book:
+            # Each sheet has name
+            print("sheet: %s" % sheet.name)
+            # Once you have a sheet instance, you can regard it as
+            # a Reader instance. You can iterate its member in the way
+            # you wanted it
+            for row in sheet:
+                print(row)
+        return book
 
-    def scan(self, address: str, tx_type: str, tx_count: int, ):
-        print("scan eth:", address, tx_type, tx_count)
+    def handle(self, ):
+        self.book_a = self.read(self.file_a)
 
-        transactions = self.client.eth.get_transaction_count(address)
-        print(f"transactions: {transactions}, wait...")
-
-        api = Account(address=address, api_key=self.etherscan_api_key)
-
-        # txs = api.get_all_transactions(offset=10, sort='asc', internal=False)
-        txs = api.get_transaction_page(offset=10, sort='desc', internal=False)
-
-        for tx in txs:
-            eth_balance = self.client.fromWei(int(tx['value']), "ether")
-            print(f"\ttx: {tx['blockNumber']} {tx['hash']} {eth_balance}, {tx['from']} -> {tx['to']}")
-            # print(f"\ttx:{tx}")
+    def write(self, data):
+        pass
 
 
 @click.command()
-@click.option("--rpc_url", default="", help="eth rpc url")
-@click.option("--etherscan_api_key", default="", help="etherscan api key")
+@click.option("--file_a", default="input/1.xlsx", help="sheet filename")
+@click.option("--file_b", default="input/2.xlsx", help="sheet filename")
 @click.option("--address", default="", help="eth address")
 @click.option("--tx_type", default="", help="eth tx type")
 @click.option("--tx_count", default=0, help="eth tx count")
-def main(rpc_url: str, etherscan_api_key: str, address: str, tx_type: str, tx_count: int, ):
-    print(f"input args: {rpc_url}, {etherscan_api_key}, {address}, {tx_type}, {tx_count}")
-    r = CSVParser(rpc_url=rpc_url, etherscan_api_key=etherscan_api_key)
-    r.balance(address)
-    r.scan(address=address, tx_type=tx_type, tx_count=tx_count)
+def main(file_a: str, file_b: str, address: str, tx_type: str, tx_count: int, ):
+    print(f"input args: {file_a}, {file_b}, {address}, {tx_type}, {tx_count}")
+    r = CSVParser(file_a=file_a, file_b=file_b)
+    r.handle()
 
 
 if __name__ == '__main__':
