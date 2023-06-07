0e238e993bc81cf119110627099c433e976ae543
Henry.Huang <hhglory@outlook.com>
2023-06-07 17:10:28 +0800

    update

--- packages/wordlist/src/wordlist/wordlist.py
+++ packages/wordlist/src/wordlist/wordlist.py
@@ -24,6 +24,24 @@ class WordListTool(object):
 
         logger.debug(f"resource_path: {self.resource_path}, dist_path: {self.dist_path}")
 
+        #
+        # vocabularies: 分级词库
+        #
+        self.words = {
+            "cet4": set(),  # 英语4级
+            "cet6": set(),  # 英语6级
+            "toefl": set(),  # 托福
+            "ielts": set(),  # 雅思
+            "gre": set(),  # GRE
+            #
+            # merge:
+            #
+            "cet4_cet6": set(),  # 英语4级 + 英语6级
+            "cet4_cet6_toefl": set(),  # 英语4级 + 英语6级 + 托福
+            "cet4_cet6_toefl_ielts": set(),  # 英语4级 + 英语6级 + 托福 + 雅思
+            "cet4_cet6_toefl_ielts_gre": set(),  # 英语4级 + 英语6级 + 托福 + 雅思 + GRE
+        }
+
     def parse_xlsx(self, filename: str):
         """解析 xlsx 文件
         :param filename: 文件名
@@ -31,7 +49,37 @@ class WordListTool(object):
         """
         pass
 
-    def parse_dir(self, dirs: str = None, file_type: str = ".png"):
+    def parse_vocabulary_by_dirs(self):
+        """解析词库文件
+
+        :param
+        :return:
+        """
+
+        self.parse_dirs()
+        self.save_files()
+
+    def save_files(self):
+        """保存文件
+        :return:
+        """
+        for k, v in self.words.items():
+            f_txt = self.dist_path.joinpath(f"{k}.txt")
+            f_json = self.dist_path.joinpath(f"{k}.json")
+
+            # sort:
+            v = sorted(v)
+
+            # save to json:
+            with open(f_json, "w") as fp:
+                fp.write(json.dumps(v))
+
+            # save to txt:
+            with open(f_txt, "w") as fp:
+                for word in v:
+                    fp.write(word + "\n")
+
+    def parse_dirs(self, dirs: str = None, file_type: str = ".png"):
         """解析目录下所有文件
 
         :param dirs: 目录路径
@@ -48,26 +96,37 @@ class WordListTool(object):
 
         for k, v in res_dirs.items():
             res_dir = self.resource_path.joinpath(v)
-            logger.debug(f"res_dir: {res_dir}")
 
             words = set()
             for f in res_dir.iterdir():
                 if f.is_file() and f.suffix == file_type:
-                    print(f"file name: {f.name}, file path: {f.absolute()}")
                     word = f.name.lower().removesuffix(file_type)
                     words.add(word)
-                logger.debug(f"words: {len(words)}, dir: {res_dir}")
 
-            #
-            #
-            #
-            f_txt = self.dist_path.joinpath(f"{k}.txt")
-            f_json = self.dist_path.joinpath(f"{k}.json")
+            logger.debug(f"words: {len(words)}, dir: {res_dir}")
+            # sorted
+            self.words[k] = sorted(words)
 
-            # save to txt:
-            with open(f_txt, "w") as fp:
-                for word in words:
-                    fp.write(word + "\n")
-            # save to json:
-            with open(f_json, "w") as fp:
-                fp.write(json.dumps(list(words)))
+        # merge words:
+        merge_words = set()
+        # add cet4
+        merge_words.update(self.words["cet4"])
+        merge_words.update(self.words["cet6"])
+
+        self.words["cet4_cet6"] = set(sorted(merge_words))
+        logger.debug(f"words union: cet4_cet6 = {len(self.words['cet4_cet6'])}")
+
+        # add toefl
+        merge_words.update(self.words["toefl"])
+        self.words["cet4_cet6_toefl"] = set(sorted(merge_words))
+        logger.debug(f"words union: cet4_cet6_toefl = {len(self.words['cet4_cet6_toefl'])}")
+
+        # add ielts
+        merge_words.update(self.words["ielts"])
+        self.words["cet4_cet6_toefl_ielts"] = set(sorted(merge_words))
+        logger.debug(f"words union: cet4_cet6_toefl_ielts = {len(self.words['cet4_cet6_toefl_ielts'])}")
+
+        # add gre
+        merge_words.update(self.words["gre"])
+        self.words["cet4_cet6_toefl_ielts_gre"] = set(sorted(merge_words))
+        logger.debug(f"words union: cet4_cet6_toefl_ielts_gre = {len(self.words['cet4_cet6_toefl_ielts_gre'])}")
--- packages/wordlist/tests/test_wordlist.py
+++ packages/wordlist/tests/test_wordlist.py
@@ -5,4 +5,4 @@ from src.wordlist.wordlist import WordListTool
 
 def test_wordlist():
     w = WordListTool()
-    w.parse_dir()
+    w.parse_vocabulary_by_dirs()
